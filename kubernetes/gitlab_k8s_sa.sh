a7fcb593fbd1aa9762cbe6fd0a535589aaa9e7ad
Ivan <61388098+cancer13@users.noreply.github.com>
2022-11-17 21:49:43 +0300

    skip auth can-i

--- kubernetes/gitlab_k8s_sa.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl apply -f gitlab-admin-service-account.yaml
-kubectl -n kube-system get secrets -o json | \
-jq -r '.items[] | select(.metadata.name | startswith("gitlab-admin")) | .data.token' | \
-base64 --decode

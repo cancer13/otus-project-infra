e49f0ac1ac26d7c05e5b13533424f01751856a35
Ivan <61388098+cancer13@users.noreply.github.com>
2022-11-24 11:35:43 +0300

    fix monitoring version

--- monitoring/helm_run-monitoring.sh
+++ monitoring/helm_run-monitoring.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh/
+export SLACK_WEBHOOK="$(cat slack_webhook.key)"
+
 helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
     -f values-kube-prometheus-stack.yaml \
+    --set alertmanager.config.global.slack_api_url=$SLACK_WEBHOOK \
     -n monitoring \
-    --create-namespace
+    --create-namespace \
+    --version="41.9.1" \
+    --timeout 10m
